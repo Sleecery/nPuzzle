194a3e878390acaadfe99c010d122b616850d3cb
Sleecery <fkonecny2@gmail.com>
2015-05-17 23:32:11 +0200

    puzzle 7

---
+++ Puzzle/PuzzleGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Puzzle.Core;
+using System.Threading;
+using Puzzle.CUI;
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Starts game
+    /// </summary>
+    class PuzzleGame
+    {
+        private ConsoleUI userInterface = null;
+
+        public PuzzleGame()
+        {
+            Field field = new Field(4, 4);
+            userInterface = new ConsoleUI(field);
+            userInterface.StartNewGame();
+        }
+        static int Main(string[] args)
+        {
+            PuzzleGame pg = new PuzzleGame();
+            return 0;
+        }
+    }
+}
